3cf4fc3a02cc12d042113275be27d6c5ec47b79e
河合拓也 <pgkichi@gmail.com>
2021-06-02 16:06:13 +0900

    better errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,14 @@
 class ArticlesController < ApplicationController
     before_action :set_article,only:[:show,:edit,:update]
-    
+
     def index
         @articles = Article.all
-    end    
+    end
     def show
-    end    
+    end
     def new
         @article = Article.new
-    end  
+    end
     def create
         @article =Article.new(article_params)
         if @article.save
@@ -16,32 +16,32 @@ class ArticlesController < ApplicationController
         else
             flash.now[:error]='保存に失敗しました'
             render:new
-                    
-        end    
-    end 
+
+        end
+    end
 
     def edit
     end
-    
+
     def update
         if @article.update(article_params)
            redirect_to article_path(@article),notice:'更新できました'
         else
             flash.now[:error]='更新できませんでした'
             render:edit
-        end    
-    end   
+        end
+    end
     def destroy
         article=Article.find(params[:id])
         article.destroy!
-        redirect_to root_path,notice:'削除に成功しました'        
-    end    
+        redirect_to root_path,notice:'削除に成功しました'
+    end
     private
     def article_params
         params.require(:article).permit(:title, :content)
-    end    
+    end
 
     def set_article
-        @article=Article.find(params[:id])  
-    end    
+        @article=Article.find(params[:id])
+    end
 end
