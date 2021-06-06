1eb66e0bc334398abc101a4a9fc4d90118e0484a
河合拓也 <pgkichi@gmail.com>
2021-06-06 18:08:23 +0900

    seed修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,10 +3,15 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
-#  content    :text
-#  title      :string
+#  content    :text             not null
+#  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
     validates :title,presence: true
@@ -19,11 +24,16 @@ class Article < ApplicationRecord
     validates :content,uniqueness: true
 
     validate :validate_title_and_content_length
-
+    belongs_to :user
     
     def display_created_at
     I18n.l(self.created_at,format: :default)
     end
+
+    def author_name
+        user.display_name
+    end    
+
     # 独自のvalidationの設定
     private
     def validate_title_and_content_length
